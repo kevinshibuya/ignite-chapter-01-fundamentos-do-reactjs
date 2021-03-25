63197cd36ee576111e0c7dddef4b2197dbb86320
Kevin Shibuya <kevinshibuya@hotmail.com>
2021-03-24 21:32:21 -0300

    understanding useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
+// https://api.github.com/users/kevinshibuya/repos
 
 const repository = {
   name: "Xcard",
@@ -9,6 +12,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/kevinshibuya/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <ul>
